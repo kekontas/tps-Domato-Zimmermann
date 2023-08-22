6b7ca9c3af63836e04830e731322e98c70c123e7
kekontas <alejozimmermann@gmail.com>
2023-08-22 17:48:06 -0300

    pruebas

--- TP1/Region.hs
+++ TP1/Region.hs
@@ -1,4 +1,4 @@
-module Region ( Region, newR, foundR, linkR {-, tunelR, pathR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR  -})
+module Region ( Region, newR, foundR, linkR , tunelR{-, pathR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR  -})
    where
 
 import City
@@ -20,10 +20,12 @@ foundR (Reg cities links tunels) city = if inRegion city (Reg cities links tunel
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
 linkR (Reg cities links tunels) city1 city2 quality = Reg cities (links ++ [newL city1 city2 quality]) tunels
 
---tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
---tunelR (Reg cities links tunels) (c1:c2) = if c1 == c2 then error "Las ciudades deben ser distintas"
-                                           --else if not(inRegion c1 (Reg cities links tunels)) || not(inRegion c2 (Reg cities links tunels)) then error "Las ciudades deben estar en la region"
-                                           --else Reg cities links (tunels ++ [newT (tunelLinks (Reg cities links tunels) (c1:c2))])
+tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
+tunelR (Reg cities links tunels) [c1, c2] = if c1 == c2 then error "Las ciudades deben ser distintas"
+                                           else if not(inRegion c1 (Reg cities links tunels)) || not(inRegion c2 (Reg cities links tunels)) then error "Las ciudades deben estar en la region"
+                                                else if not(linkedR (Reg cities links tunels) c1 c2) then error "Las ciudades deben estar conectadas"
+                                                     else Reg cities links (tunels ++ [newT [(whichLink (Reg cities links tunels) c1 c2)]])
+
 {-connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 
 zinkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
@@ -39,3 +41,16 @@ inRegion city (Reg cities links tunels) = city `elem` cities
 equalR :: Region -> Region -> Bool
 equalR (Reg cities1 links1 tunels1) (Reg cities2 links2 tunels2) = (cities1 == cities2) && (links1 == links2) && (tunels1 == tunels2)
 
+linkedR :: Region -> City -> City -> Bool
+linkedR (Reg cities (l1:ls) tunels) city1 city2 = if linksL city1 city2 l1 then True
+                                                   else linkedR (Reg cities ls tunels) city1 city2
+linkedR (Reg cities [] tunels) city1 city2 = False
+
+whichLink :: Region -> City -> City -> Link
+whichLink (Reg cities (l1:ls) tunels) city1 city2 = if length ls == 0   then if linksL city1 city2 l1 then l1
+                                                         else whichLink (Reg cities ls tunels) city1 city2
+                                                    else if linksL city1 city2 l1
+                                                         then l1
+                                                         else whichLink (Reg cities ls tunels) city1 city2
+                                                      
+whichLink (Reg cities [] tunels) city1 city2 = error "Las ciudades no estan conectadas"                                                     
--- TP1/pruebas.hs
+++ TP1/pruebas.hs
@@ -3,12 +3,13 @@ import City
 import Link 
 import Quality 
 import Tunel 
-p1 = newP 1 2 
-c1 = newC "Hola" p1 
-p2 = newP 1 3
-c2 = newC "Gil" p2 
+import Region
+ 
+c1 = newC "Hola" (newP 1 2) 
+c2 = newC "Gil" (newP 1 3) 
 q1 = newQ "Cobre" 18 0.5 
 l1 = newL c1 c2 q1 
-p3 = newP 1 4
-c3 = newC "pun" p3
-t1 = newT [l1]
+c3 = newC "pun" (newP 1 4)
+c4 = newC "pun" (newP 1 5)
+l2 = newL c3 c4 q1
+t1 = newT [l1, l2]
