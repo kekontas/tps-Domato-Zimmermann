3e6fa9a320025239e74a207785d7f00641c5b197
sdomato <santino.domato@gmail.com>
2023-11-01 21:19:58 -0300

    Game

--- TP4/_4_En_Linea/src/linea/Game_test.java
+++ TP4/_4_En_Linea/src/linea/Game_test.java
@@ -1,52 +1,97 @@
 package linea;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Game_test {
     @Test
-    public void test00CreatNewBoard() {
-        Linea game = new Linea( 10, 10, 'C' );
-        assertEquals( 10, game.numRows );
-        assertEquals( 10, game.numCols );
+    public void test00CreateNewBoard() {
+        Linea game = new Linea(10, 10, 'c');
+        assertEquals(10, game.numRows);
+        assertEquals(10, game.numCols);
     }
+
     @Test
     public void test01PlayRedAt() {
-        Linea game = new Linea( 10, 10, 'C' );
-        game.playRedAt( 5 );
-        assertEquals( 'C', game.isPlayerAt( 5, 9 ) );
+        Linea game = new Linea(10, 10, 'c');
+        game.playRedAt(5);
+        assertEquals('R', game.isPlayerAt(5, 9));
     }
+
     @Test
     public void test02PlayBlueAt() {
-        Linea game = new Linea( 10, 10, 'C' );
-        game.playBlueAt( 5 );
-        assertEquals( 'B', game.isPlayerAt( 5, 9 ) );
+        Linea game = new Linea(10, 10, 'c');
+        game.playBlueAt(5);
+        assertEquals('B', game.isPlayerAt(5, 9));
     }
+
     @Test
     public void test03RedCantPlay2InARow() {
-        Linea game = new Linea( 10, 10, 'C' );
-        game.playRedAt( 5 );
-        assertThrows( RuntimeException.class, () -> game.playRedAt( 5 ) );
+        Linea game = new Linea(10, 10, 'c');
+        game.playRedAt(5);
+        assertThrows(IllegalArgumentException.class, (Executable) () -> game.playRedAt(5));
     }
+
     @Test
     public void test04BlueCantPlay2InARow() {
-        Linea game = new Linea( 10, 10, 'C' );
-        game.playBlueAt( 5 );
-        assertThrows( RuntimeException.class, () -> game.playBlueAt( 5 ) );
-
+        Linea game = new Linea(10, 10, 'c');
+        game.playBlueAt(5);
+        assertThrows(IllegalArgumentException.class, (Executable) () -> game.playBlueAt(5));
     }
+
     @Test
     public void test05RedCantPlayOutOfBoard() {
-        Linea game = new Linea( 10, 10, 'C' );
-        assertThrows( RuntimeException.class, () -> game.playRedAt( 10 ) );
-
+        Linea game = new Linea(10, 10, 'c');
+        assertThrows(IllegalArgumentException.class, (Executable) () -> game.playRedAt(10));
     }
+
     @Test
     public void test06BlueCantPlayOutOfBoard() {
-        Linea game = new Linea( 10, 10, 'C' );
-        assertThrows( RuntimeException.class, () -> game.playBlueAt( 10 ) );
-
+        Linea game = new Linea(10, 10, 'c');
+        assertThrows(IllegalArgumentException.class, (Executable) () -> game.playBlueAt(10));
     }
 }
+
+   /* @Test
+    // testea que un jugador puede ganar en horizontal
+    public void test07RedWinHorizontal() {
+        Linea game = new Linea( 10, 10, 'C' );
+        game.playRedAt( 0 );
+        game.playRedAt( 1 );
+        game.playRedAt( 2 );
+        game.playRedAt( 3 );
+        assertTrue( game.finished() );
+        assertEquals( 'R', game.winner() );
+    }
+    @Test
+    // testea que un jugador puede ganar en vertical
+    public void test08RedWinVertical() {
+        Linea game = new Linea( 10, 10, 'C' );
+        game.playRedAt( 0 );
+        game.playRedAt( 0 );
+        game.playRedAt( 0 );
+        game.playRedAt( 0 );
+        assertTrue( game.finished() );
+        assertEquals( 'R', game.winner() );
+    }
+    @Test
+    // testea que un jugador puede ganar en diagonal
+    public void test09RedWinDiagonal() {
+        Linea game = new Linea( 10, 10, 'C' );
+        game.playRedAt( 0 );
+        game.playBlueAt( 1 );
+        game.playRedAt( 1 );
+        game.playBlueAt( 2 );
+        game.playBlueAt( 2 );
+        game.playRedAt( 2 );
+        game.playBlueAt( 3 );
+        game.playBlueAt( 3 );
+        game.playBlueAt( 3 );
+        game.playRedAt( 3 );
+        assertTrue( game.finished() );
+        assertEquals( 'R', game.winner() );
+    }*/
+
--- TP4/_4_En_Linea/src/linea/Linea.java
+++ TP4/_4_En_Linea/src/linea/Linea.java
@@ -1,78 +1,116 @@
 package linea;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Linea {
-    char[][] board;
+    private List<List<Character>> board;
     int numRows;
     int numCols;
     private char currentPlayer;
 
-    public Linea(int numRows, int numCols, char startPlayer) {
+    public Linea(int numRows, int numCols, char gameMode) {
         this.numRows = numRows;
         this.numCols = numCols;
-        this.board = new char[numRows][numCols];
-        this.currentPlayer = startPlayer;
+        this.board = new ArrayList<>();
+
+        for (int i = 0; i < numCols; i++) {
+            List<Character> column = new ArrayList<>();
+            for (int j = 0; j < numRows; j++) {
+                column.add('\0'); // Inicializa con caracteres nulos
+            }
+            this.board.add(column);
+        }
+
+        if (gameMode == 'c') {
+            // Decide quién comienza el juego en función del modo
+            this.currentPlayer = 'R'; // Por ejemplo, 'R' para jugador rojo, 'B' para jugador azul
+        } else {
+            throw new IllegalArgumentException("Modo de juego no válido");
+        }
     }
-    public String show() {
-        StringBuilder sb = new StringBuilder();
+
+    public boolean show() {
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
-                sb.append(board[row][col]).append(' ');
+                System.out.print(board.get(col).get(row) + " ");
             }
-            sb.append('\n');
+            System.out.println(); // Nueva línea para la siguiente fila
         }
-        return sb.toString();
-    }
-    public boolean finished() {
-
-        // Horizontal
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols - 3; col++) {
-                if (board[row][col] != 0 &&
-                        board[row][col] == board[row][col + 1] &&
-                        board[row][col] == board[row][col + 2] &&
-                        board[row][col] == board[row][col + 3]) {
-                    return true;
-                }
-            }
-        }
-
-        // Vertical
-        for (int row = 0; row < numRows - 3; row++) {
-            for (int col = 0; col < numCols; col++) {
-                if (board[row][col] != 0 &&
-                        board[row][col] == board[row + 1][col] &&
-                        board[row][col] == board[row + 2][col] &&
-                        board[row][col] == board[row + 3][col]) {
-                    return true;
-                }
-            }
-        }
-
-        // Diagonal
-        for (int row = 0; row < numRows - 3; row++) {
-            for (int col = 0; col < numCols - 3; col++) {
-                if (board[row][col] != 0 &&
-                        board[row][col] == board[row + 1][col + 1] &&
-                        board[row][col] == board[row + 2][col + 2] &&
-                        board[row][col] == board[row + 3][col + 3]) {
-                    return true;
-                }
-            }
-        }
-
-        // Diagonal
-        for (int row = 0; row < numRows - 3; row++) {
-            for (int col = 3; col < numCols; col++) {
-                if (board[row][col] != 0 &&
-                        board[row][col] == board[row + 1][col - 1] &&
-                        board[row][col] == board[row + 2][col - 2] &&
-                        board[row][col] == board[row + 3][col - 3]) {
-                    return true;
-                }
-            }
-        }
-
         return false;
+    }
+
+    public boolean finished() {
+        return checkWin() || isBoardFull();
+    }
+
+    private boolean checkWin() {
+        return checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin();
+    }
+
+    private boolean checkHorizontalWin() {
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col <= numCols - 4; col++) {
+                char cell = board.get(col).get(row);
+                if (cell != '\0' &&
+                        cell == board.get(col + 1).get(row) &&
+                        cell == board.get(col + 2).get(row) &&
+                        cell == board.get(col + 3).get(row)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkVerticalWin() {
+        for (int row = 0; row <= numRows - 4; row++) {
+            for (int col = 0; col < numCols; col++) {
+                char cell = board.get(col).get(row);
+                if (cell != '\0' &&
+                        cell == board.get(col).get(row + 1) &&
+                        cell == board.get(col).get(row + 2) &&
+                        cell == board.get(col).get(row + 3)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonalWin() {
+        for (int row = 0; row <= numRows - 4; row++) {
+            for (int col = 0; col <= numCols - 4; col++) {
+                char cell = board.get(col).get(row);
+                if (cell != '\0' &&
+                        cell == board.get(col + 1).get(row + 1) &&
+                        cell == board.get(col + 2).get(row + 2) &&
+                        cell == board.get(col + 3).get(row + 3)) {
+                    return true;
+                }
+            }
+        }
+
+        for (int row = 0; row <= numRows - 4; row++) {
+            for (int col = 3; col < numCols; col++) {
+                char cell = board.get(col).get(row);
+                if (cell != '\0' &&
+                        cell == board.get(col - 1).get(row + 1) &&
+                        cell == board.get(col - 2).get(row + 2) &&
+                        cell == board.get(col - 3).get(row + 3)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int col = 0; col < numCols; col++) {
+            if (board.get(col).contains('\0')) {
+                return false; // Todavía hay casillas vacías
+            }
+        }
+        return true; // El tablero está lleno, empate
     }
 
     public void playRedAt(int col) {
@@ -88,36 +126,42 @@ public class Linea {
             throw new IllegalArgumentException("Invalid column: " + col);
         }
 
-        if (board[0][col] != 0) {
+        List<Character> column = board.get(col);
+
+        if (!column.contains('\0')) {
             throw new IllegalArgumentException("Column is full: " + col);
         }
 
         for (int row = numRows - 1; row >= 0; row--) {
-            if (board[row][col] == 0) {
-                board[row][col] = player;
+            if (column.get(row) == '\0') {
+                column.set(row, player);
                 switchPlayer();
                 return;
             }
         }
     }
 
-    public Boolean casilleroIsEmpty(int col, int row) {
-        if (board[row][col] == 0) {
-            return true;
-        }
-        return false;
+    public boolean casilleroIsEmpty(int col, int row) {
+        return board.get(col).get(row) == '\0';
     }
-    public int isPlayerAt(int col, int row) {
-        return board[row][col];
+
+    public char isPlayerAt(int col, int row) {
+        return board.get(col).get(row);
     }
-    private void switchPlayer () {
+
+    private void switchPlayer() {
         if (currentPlayer == 'R') {
             currentPlayer = 'B';
         } else {
             currentPlayer = 'R';
         }
     }
+
+    public char winner() {
+        return currentPlayer;
+    }
 }
+
 
 
 
